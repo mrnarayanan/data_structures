4aa6303e729ac53be8c016f95bca25bdc3830018
mrn2 <mrn2@illinois.edu>
2018-01-25 12:42:12 -0600

    Finished lab_debug

--- lab_debug/sketchify.cpp
+++ lab_debug/sketchify.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <cmath>
+#include <iostream>
 
 #include "cs225/PNG.h"
 #include "cs225/HSLAPixel.h"
@@ -13,29 +14,28 @@ PNG* setupOutput(unsigned w, unsigned h) {
 
 // Returns my favorite color
 HSLAPixel* myFavoriteColor(double saturation) {
-    HSLAPixel pixel(-1, saturation, 0.5);
-    return &pixel;
+    HSLAPixel* pixel = new HSLAPixel(240, saturation, 0.5); // changed color
+    return pixel;
 }
 
 void sketchify(std::string inputFile, std::string outputFile) {
     // Load in.png
-    PNG* original = NULL;
-
+    PNG* original = new PNG();
+    std::cout << "Reached Line " << __LINE__ << std::endl;
     original->readFromFile(inputFile);
     unsigned width = original->width();
     unsigned height = original->height();
-
+    std::cout << "Reached Line " << __LINE__ << std::endl;
     // Create out.png
-    PNG* output;
-    setupOutput(width, height);
+    PNG* output = setupOutput(width, height);
 
     // Load our favorite color to color the outline
     HSLAPixel* myPixel = myFavoriteColor(0.5);
 
     // Go over the whole image, and if a pixel differs from that to its upper
     // left, color it my favorite color in the output
-    for (unsigned y = 1; 0 < y < height; y++) {
-        for (unsigned x = 1; 0 < x < width; x++) {
+    for (unsigned y = 1; y < height; y++) {
+        for (unsigned x = 1; x < width; x++) {
             // Calculate the pixel difference
             HSLAPixel & prev = original->getPixel(x - 1, y - 1);
             HSLAPixel & curr = original->getPixel(x, y);
@@ -45,7 +45,7 @@ void sketchify(std::string inputFile, std::string outputFile) {
             // color the output pixel with my favorite color
             HSLAPixel * currOutPixel = &(output->getPixel(x, y));
             if (diff > 20) {
-                currOutPixel = myPixel;
+                *currOutPixel = *myPixel;
             }
         }
     }
