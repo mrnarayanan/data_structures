2ee675463a033c65345da9e62be4015ac1455b1f
mrn2 <mrn2@illinois.edu>
2018-02-27 23:16:07 -0600

    updated main for testing

--- mp3/main.cpp
+++ mp3/main.cpp
@@ -3,13 +3,13 @@
 int main() {
   List<int> mylist, list2;
   mylist.insertBack(1);
+  mylist.insertBack(3);
   mylist.insertBack(2);
-  mylist.insertBack(5);
+  mylist.insertBack(10);
+  mylist.insertBack(6);
+  mylist.insertBack(9);
   mylist.insertBack(7);
-  mylist.insertBack(12);
-  mylist.insertBack(15);
-  mylist.insertBack(17);
-  mylist.insertBack(19);
+  mylist.insertBack(4);
 
   list2.insertBack(8);
   list2.insertBack(9);
@@ -19,7 +19,7 @@ int main() {
   std::cout << std::endl;
   mylist.print(std::cout);
   std::cout << std::endl;
-  mylist.mergeWith(list2);
+  mylist.sort();
   mylist.print(std::cout);
   std::cout << std::endl;
 
