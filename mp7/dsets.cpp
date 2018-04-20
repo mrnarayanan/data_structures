95921c93dd0b548f64439355deb03b0cf71d0149
mrn2 <mrn2@illinois.edu>
2018-04-20 01:59:47 -0500

    MP7 Part 1 complete

--- mp7/dsets.cpp
+++ mp7/dsets.cpp
@@ -1,1 +1,43 @@
 /* Your code here! */
+#include "dsets.h"
+
+void DisjointSets::addelements(int num)
+{
+  for (int i = 0; i < num; i++)
+    arr_.push_back(-1);
+}
+
+int DisjointSets::find(int i)
+{
+  if (arr_[i] < 0)
+    return i;
+  else
+    return find(arr_[i]);
+}
+
+void DisjointSets::setunion(int a, int b)
+{
+  // find roots
+  int root1 = find(a);
+  int root2 = find(b);
+
+  int newSize = arr_[root1] + arr_[root2];
+
+  // if arr_[root1] is less than (more negative), it is larger, so smaller gets unioned to it
+  if (arr_[root1] <= arr_[root2])
+  {
+    arr_[root2] = root1;
+    arr_[root1] = newSize;
+  }
+  else
+  {
+    arr_[root1] = root2;
+    arr_[root2] = newSize;
+  }
+}
+
+int DisjointSets::size(int elem)
+{
+  int root = find(elem);
+  return arr_[root];
+}
--- mp7/dsets.h
+++ mp7/dsets.h
@@ -1,1 +1,21 @@
 /* Your code here! */
+#ifndef DSETS_H_
+#define DSETS_H_
+
+#include <vector>
+
+using namespace std;
+
+class DisjointSets
+{
+public:
+  void addelements(int num);
+  int find(int elem);
+  void setunion(int a, int b);
+  int size(int elem);
+private:
+  std::vector<int> arr_;
+};
+
+
+#endif
